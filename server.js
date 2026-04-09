784c0026ff31a2f7b27d522332a981b2f10f5976
Drivery <jarnorfilms@gmail.com>
2026-04-09 18:47:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const Groq = require('groq-sdk');
+const { ejecutarDespacho } = require('./automator.js');
 require('dotenv').config();
 
 const app = express();
